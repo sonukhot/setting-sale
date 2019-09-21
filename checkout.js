60eb47b0c0029738fcebb151e5fb1264396d16c6
unknown <ujwala.pawnarkar@gmail.com>
2019-09-21 15:42:22 -0400

    using filter implemented function selectsalesitem by pulling items that are discounted

--- checkout.js
+++ checkout.js
@@ -1,5 +1,7 @@
 function selectSaleItems(items) {
-  return items
+  return items.filter(function discount(items) {
+    return items.discount > 0
+  })
 }
-
+//filter out discounts
 module.exports = selectSaleItems
